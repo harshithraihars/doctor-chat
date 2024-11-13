96c411a2c7ad464c588da48a2e43187977ed0310
harshithraihars <harshithraiharsu@gmail.com>
2024-11-13 15:58:53 +0530

    last

--- frontend/src/pages/HealthBot.jsx
+++ frontend/src/pages/HealthBot.jsx
@@ -9,34 +9,38 @@ import { HiDotsVertical } from "react-icons/hi";
 import { IoSearch } from "react-icons/io5";
 import { useAuth } from "../contexts/AuthContext";
 import { docName, docRole, senderId } from "../contexts/Socket";
-
+import { ClipLoader } from "react-spinners";
 const HealthBot = () => {
   const send = new Audio(sendSound);
   const messagesound = new Audio(messsageSound);
   const { specialist, socket, user, doctor } = useAuth();
-  const [doctorName,setDocname]=useState(null)
-  const [chat,setChat]=useState({})
-  const [currentClient,setcurrentClient]=useState(null)
+  const [doctorName, setDocname] = useState(null);
+  const [chat, setChat] = useState({});
+  const [currentClient, setcurrentClient] = useState(null);
   // const [doctorSenderId, setDoctorSenderId] = useState(null);
-  const [clientId, setClientId] = useState(null);
-  const isSendingRef = useRef(false);
+  // const [clientId, setClientId] = useState(null);
+  // const isSendingRef = useRef(false);
   const [clientName, setClientName] = useState(null);
   const [input, setInput] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
   const messagesEndRef = useRef(null);
-  const clientIdRef = useRef(null);
-  const [selectedClient,setSelectedClient]=useState(null)
+  // const clientIdRef = useRef(null);
+  const [selectedClient, setSelectedClient] = useState(null);
   // const doctorSenderIdRef = useRef(null);
-  const [doctorchat,setDoctorchat]=useState([])
+  const [doctorchat, setDoctorchat] = useState([]);
+  const [unreadMessages, setUnreadMessages] = useState({});
   let sender, reciever;
-  useEffect(()=>{
-    setDocname(docName)
-  },[docName])
+
+  useEffect(() => {
+    setDocname(docName);
+  }, [docName]);
+
   useEffect(() => {
     if (doctor) {
       localStorage.setItem("docSend", senderId);
     }
   }, []);
+
   useEffect(() => {
     if (user) {
       socket.on("user-connected", ({ id }) => {
@@ -49,28 +53,53 @@ const HealthBot = () => {
 
   useEffect(() => {
     sender = localStorage.getItem("sender");
-    setcurrentClient(sender)
+    setcurrentClient(sender);
     reciever = localStorage.getItem("reciever");
   }, [sender, reciever]);
+
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
     if (socket) {
-      const handleReceive = ({ message, sendId, recieverName,recieverId }) => {
+      const handleReceive = ({ message, sendId, recieverName, recieverId }) => {
         if (doctor) {
-          if(!selectedClient){
-            console.log(true);
-            setSelectedClient(sendId)
+          if (!selectedClient) {
+            setSelectedClient(sendId);
           }
           setDoctorchat((prevchat) => {
-            const isExist = prevchat.find((client) => client.socketId === sendId);
+            const isExist = prevchat.find(
+              (client) => client.socketId === sendId
+            );
             if (!isExist) {
-              return [...prevchat, { socketId: sendId, clientName: recieverName, lastMessage: "i want a favour" }];
+              return [
+                ...prevchat,
+                {
+                  socketId: sendId,
+                  clientName: recieverName,
+                  lastMessage: "i want a favour",
+                },
+              ];
             }
             return prevchat;
           });
+
+          if (selectedClient !== sendId) {
+            setDoctorchat((prevchat) => {
+              return prevchat.map((chat) => {
+                if (chat.socketId === sendId) {
+                  return { ...chat, lastMessage: message };
+                }
+                return chat;
+              });
+              
+            });
+            setUnreadMessages((prevCounts) => ({
+              ...prevCounts,
+              [sendId]: (prevCounts[sendId] || 0) + 1,
+            }));
+          }
         }
         messagesound.currentTime = 0;
         messagesound.play();
@@ -80,32 +109,30 @@ const HealthBot = () => {
         } else {
           if (!clientName) {
             console.log("not");
-            
+
             setClientName(recieverName);
           }
-          if(!currentClient){
+          if (!currentClient) {
             // setcurrentClient(recieverId)*
-            setcurrentClient(sendId)
+            setcurrentClient(sendId);
           }
-          setClientId(sendId);
-          clientIdRef.current = sendId;
+          // setClientId(sendId);
+          // clientIdRef.current = sendId;
         }
         const userMessage = { text: message, isBot: true };
-        let chatIndex
-        if(user){
-          chatIndex=senderId
-        }
-        else{
+        let chatIndex;
+        if (user) {
+          chatIndex = senderId;
+        } else {
           // chatIndex=currentClient?currentClient:sendId
-          console.log(selectedClient);
-          chatIndex=sendId
+          // console.log(selectedClient);
+          chatIndex = sendId;
         }
-        
-        setChat((prevChat)=>(
-          {...prevChat,
-            [chatIndex]:[...(prevChat[chatIndex]) || [],userMessage]
-          }
-        ))
+
+        setChat((prevChat) => ({
+          ...prevChat,
+          [chatIndex]: [...(prevChat[chatIndex] || []), userMessage],
+        }));
       };
 
       socket.on("receive", handleReceive);
@@ -115,25 +142,18 @@ const HealthBot = () => {
         socket.off("receive", handleReceive);
       };
     }
-  }, [socket, user,clientName]);
-
-  useEffect(()=>{
-    console.log(chat); 
-  },[chat])
+  }, [socket, user, clientName]);
 
   const handleSendMessage = async () => {
     if (!input.trim()) return;
 
     const userMessage = { text: input, isBot: false };
-    console.log(currentClient)
-    let sendIndex=user?currentClient:selectedClient
-    setChat((prevChat)=>(
-      {...prevChat,
-        // [currentClient]:[...(prevChat[currentClient]) || [],userMessage]
-        [sendIndex]:[...(prevChat[sendIndex]) || [],userMessage]
-
-      }
-    ))
+    let sendIndex = user ? currentClient : selectedClient;
+    setChat((prevChat) => ({
+      ...prevChat,
+      // [currentClient]:[...(prevChat[currentClient]) || [],userMessage]
+      [sendIndex]: [...(prevChat[sendIndex] || []), userMessage],
+    }));
 
     if (socket) {
       reciever = localStorage.getItem("reciever");
@@ -154,12 +174,17 @@ const HealthBot = () => {
       send.currentTime = 0;
       send.play();
     }
-  }; 
+  };
 
-  const handleClient=(chat)=>{
-    setSelectedClient(chat.socketId)
-    setClientName(chat.clientName)
-  }
+  const handleClient = (chat) => {
+    setSelectedClient(chat.socketId);
+    setClientName(chat.clientName);
+    setUnreadMessages((prevCount) => ({
+      ...prevCount,
+      [chat.socketId]: 0,
+    }));
+  };
+
   return (
     <div className="flex flex-col lg:flex-row w-screen">
       {/* Profile Box */}
@@ -191,109 +216,148 @@ const HealthBot = () => {
       </div>
 
       {/* Main Chat Container */}
-      <div className={`container mx-auto px-4 py-8 ${doctor ? "lg:w-[1000px]":"lg:w-[700px]"}`}>
-        <div className="flex  mx-auto bg-white rounded-lg shadow-md overflow-hidden">
-          {/* Sidebar for Doctor Chats */}
-          {doctor ? (
-            <div className="w-2/4 border-r border-gray-200 p-4 ">
-              <div className="flex items-center justify-between">
-                <h3 className="font-bold text-left mb-4 text-gray-600">
-                  Chats
-                </h3>
-                <IoSearch size={20} className="mb-2 hover:cursor-pointer" />
-              </div>
-              {doctorchat.map((chat, index) => (
-                <div onClick={()=>handleClient(chat)}
-                  key={index}
-                  className={` ${selectedClient===chat.socketId ? "bg-[#64F9FA]":"bg-white"} p-3 rounded-lg mb-2 shadow-md border border-gray-200 cursor-pointer
-                  transition duration-300 ease-in-out hover:bg-[#64F9FA]`}
-                >
-                  <p className="font-semibold">{chat.clientName}</p>
-                  <p className="text-gray-500 text-sm truncate">
-                    {chat.lastMessage}
-                  </p>
+      <div
+        className={`container mx-auto px-4 py-8 ${
+          doctor ? "lg:w-[1000px]" : "lg:w-[700px]"
+        }`}
+      >
+        {doctor && !selectedClient ? (
+          <div className="w-full flex justify-center items-center mt-40 flex-col gap-6 animate-fadeIn">
+            <p className="text-green-800 text-2xl font-semibold animate-pulse tracking-wider">
+              Finding clients...
+            </p>
+            <ClipLoader
+              color="#36d7b7"
+              loading={true}
+              size={100}
+              cssOverride={{ borderWidth: "8px" }}
+            />
+          </div>
+        ) : (
+          <div>
+            <div className="flex  mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+              {/* Sidebar for Doctor Chats */}
+              {doctor ? (
+                <div className="w-2/4 border-r border-gray-200 p-4 ">
+                  <div className="flex items-center justify-between">
+                    <h3 className="font-bold text-left mb-4 text-gray-600">
+                      Chats
+                    </h3>
+                    <IoSearch size={20} className="mb-2 hover:cursor-pointer" />
+                  </div>
+                  {doctorchat.map((chat, index) => (
+                    <div onClick={() => handleClient(chat)}
+                    key={index}
+                    className={` ${
+                      selectedClient === chat.socketId
+                        ? "bg-[#64F9FA]"
+                        : "bg-white"
+                    } p-3 rounded-lg mb-2 shadow-md border border-gray-200 cursor-pointer
+              transition duration-300 ease-in-out hover:bg-[#64F9FA] flex justify-between items-center`}>
+                      <div>
+                        <p className="font-semibold">{chat.clientName}</p>
+                        <p className="text-gray-500 text-sm truncate">
+                          {chat.lastMessage}
+                        </p>
+                        
+                      </div>
+                      <div>
+                      {unreadMessages[chat.socketId] && selectedClient!==chat.socketId? (
+                          <p className="text-white rounded-xl mr-4 h-6 w-6 text-center bg-[#64F9FA]">
+                            {unreadMessages[chat.socketId]}
+                          </p>
+                        ) : (
+                          ""
+                        )}
+                        </div>
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          ) : (
-            ""
-          )}
+              ) : (
+                ""
+              )}
 
-          {/* Chat Area */}
-          <div className="w-2/3 lg:w-[700px] rounded-xl">
-            <div className="bg-[#64f9fa] text-black px-3 py-3 flex justify-between items-center">
-              <div className="flex items-center gap-2">
-                <button className="text-xl font-bold text-gray-700">
-                  <FaArrowLeft size={30} />
-                </button>
+              {/* Chat Area */}
+              <div className="w-2/3 lg:w-[700px] rounded-xl">
+                <div className="bg-[#64f9fa] text-black px-3 py-3 flex justify-between items-center">
+                  <div className="flex items-center gap-2">
+                    <button className="text-xl font-bold text-gray-700">
+                      <FaArrowLeft size={30} />
+                    </button>
 
-                <div className="flex items-center">
-                  <div className="h-12 w-12 bg-slate-50 rounded-full">
-                    <img
-                      src={`${doctor ? userImg : docImg}`}
-                      alt="doctor"
-                      className="rounded-full"
+                    <div className="flex items-center">
+                      <div className="h-12 w-12 bg-slate-50 rounded-full">
+                        <img
+                          src={`${doctor ? userImg : docImg}`}
+                          alt="doctor"
+                          className="rounded-full"
+                        />
+                      </div>
+                      <div className="pl-2">
+                        <h2 className="text-xl font-semibold">
+                          {doctor ? clientName || "" : doctorName || ""}
+                        </h2>
+                        {user && (
+                          <p className="text-sm text-gray-600">{docRole}</p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+
+                  <button className="text-xl font-bold text-gray-700">
+                    <HiDotsVertical size={25} className="text-black" />
+                  </button>
+                </div>
+                <div className="h-96 overflow-y-auto p-6">
+                  {(chat[user ? currentClient : selectedClient] || []).map(
+                    (message, index) => (
+                      <div
+                        key={index}
+                        className={`mb-4 ${
+                          message.isBot ? "text-left" : "text-right"
+                        }`}
+                      >
+                        <div
+                          className={`inline-block p-3 rounded-lg ${
+                            message.isBot
+                              ? "bg-gray-100"
+                              : "bg-blue-500 text-white"
+                          }`}
+                        >
+                          {message.text}
+                        </div>
+                      </div>
+                    )
+                  )}
+                  <div ref={messagesEndRef} />
+                </div>
+                <div className="border-t p-4">
+                  <div className="flex items-center">
+                    <input
+                      type="text"
+                      className="flex-grow mr-4 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 border-blue-400"
+                      placeholder="Type your message..."
+                      value={input}
+                      onChange={(e) => setInput(e.target.value)}
+                      onKeyPress={(e) =>
+                        e.key === "Enter" && handleSendMessage()
+                      }
                     />
-                  </div>
-                  <div className="pl-2">
-                    <h2 className="text-xl font-semibold">
-                      {doctor ? clientName || "" : doctorName || ""}
-                    </h2>
-                    {user && <p className="text-sm text-gray-600">{docRole}</p>}
-                  </div>
-                </div>
-              </div>
-
-              <button className="text-xl font-bold text-gray-700">
-                <HiDotsVertical size={25} className="text-black" />
-              </button>
-            </div>
-            <div className="h-96 overflow-y-auto p-6">
-              {(chat[user?currentClient:selectedClient] || []).map((message, index) => (
-                <div
-                  key={index}
-                  className={`mb-4 ${
-                    message.isBot ? "text-left" : "text-right"
-                  }`}
-                >
-                  <div
-                    className={`inline-block p-3 rounded-lg ${
-                      message.isBot ? "bg-gray-100" : "bg-blue-500 text-white"
-                    }`}
-                  >
-                    {message.text}
+                    <button
+                      className="bg-blue-500 text-white p-2 rounded-full hover:bg-blue-600 transition duration-300"
+                      onClick={handleSendMessage}
+                    >
+                      <Send size={20} />
+                    </button>
                   </div>
                 </div>
-              ))}
-              <div ref={messagesEndRef} />
-            </div>
-            <div className="border-t p-4">
-              <div className="flex items-center">
-                <input
-                  type="text"
-                  className="flex-grow mr-4 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 border-blue-400"
-                  placeholder="Type your message..."
-                  value={input}
-                  onChange={(e) => setInput(e.target.value)}
-                  onKeyPress={(e) => e.key === "Enter" && handleSendMessage()}
-                />
-                <button
-                  className="bg-blue-500 text-white p-2 rounded-full hover:bg-blue-600 transition duration-300"
-                  onClick={handleSendMessage}
-                >
-                  <Send size={20} />
-                </button>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default HealthBot;
-
-
-
-
